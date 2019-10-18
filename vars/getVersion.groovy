a84e6434817b43180c617d2d9eb2d66b87c873b2
Robert Burgess <rbburgess@gmail.com>
2019-10-17 19:05:32 -0700

    Code change

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -5,7 +5,7 @@ def call() {
         def file = readFile "conanfile.py"
         def lines = file.split("\n")
         String versionLine
-        for (int i = 0; i< lines.,size();i++){
+        for (int i = 0; i< lines.size();i++){
             if(lines[i] =~ /(   version) ?= ?['"].+['"]/){
                 versionLine = lines[i]
             }
