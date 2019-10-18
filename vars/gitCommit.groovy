5a48b6252ceeb90f0d3609d1b0ae11653765d845
Robert Burgess <rbburgess@gmail.com>
2019-10-18 05:29:59 -0700

    Code change

--- vars/gitCommit.groovy
+++ vars/gitCommit.groovy
@@ -1,4 +1,6 @@
 def call(String commitMessage) {
+    sh 'git config user.email "test@gmail.com"'
+    sh 'git config user.name "Robert"'
     sh 'git add --all'
     sh "git commit -m \"${commitMessage}\""
     sh 'git push' 
